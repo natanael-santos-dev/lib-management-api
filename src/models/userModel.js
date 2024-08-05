14858c2a462e43576d6f2445362963344f0f49ee
natanael-santos-dev <natanael.santos.dev@gmail.com>
2024-08-05 20:41:16 -0300

    feat: implements user model

---
+++ src/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        id: { type: mongoose.Schema.Types.ObjectId },
+        name: { type: String, required: true },
+        tel: { type: String, required: true },
+        email: { type: String }
+    },
+    { versionKey: false }
+);
+
+const userModel = mongoose.model('users', userSchema);
+
+export default userModel;
