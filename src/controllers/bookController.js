e8df917e99c77de68d73b297b13cf69e5f84b88d
natanael-santos-dev <natanael.santos.dev@gmail.com>
2024-08-06 09:24:33 -0300

    feat: add route to fetch  books by id

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,13 +1,25 @@
 import bookModel from '../models/bookModel.js';
 
 class BookController {
-    static async getBook(req, res) {
+    static async getBooks(req, res) {
         try {
             const books = await bookModel.find({});
             res.status(200).json(books);
         } catch (error) {
             res.status(500).json({
                 message: `${error.message} - falid to search books`
+            });
+        }
+    }
+
+    static async getBook(req, res) {
+        try {
+            const id = req.params.id;
+            const book = await bookModel.findById(id);
+            res.status(200).json(book);
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message} - falid to search book`
             });
         }
     }
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,6 +3,8 @@ import BookController from '../controllers/bookController.js';
 
 const bookRoutes = express.Router();
 
-bookRoutes.get('/books', BookController.getBook);
+bookRoutes.get('/books', BookController.getBooks);
+
+bookRoutes.get('/books/:id', BookController.getBook);
 
 export default bookRoutes;
