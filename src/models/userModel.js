c914137933130927e3ad2d755efcca7652a805e6
natanael-santos-dev <natanael.santos.dev@gmail.com>
2024-08-06 08:44:52 -0300

    refactor(user): rename property 'tel' to 'phone'

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
     {
         id: { type: mongoose.Schema.Types.ObjectId },
         name: { type: String, required: true },
-        tel: { type: String, required: true },
+        phone: { type: String, required: true },
         email: { type: String }
     },
     { versionKey: false }
