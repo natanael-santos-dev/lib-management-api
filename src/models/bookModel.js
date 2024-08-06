147d30f16b1c07e1257b9665ff0f6d4e4acf6e40
natanael-santos-dev <natanael.santos.dev@gmail.com>
2024-08-06 08:56:57 -0300

    feat: implements book model

---
+++ src/models/bookModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const bookSchema = new mongoose.Schema(
+    {
+        id: { type: mongoose.Schema.Types.ObjectId },
+        title: { type: String, require: true },
+        author: { type: String },
+        availableCopies: { type: Number, require: true }
+    },
+    { versionKey: false }
+);
+
+const bookModel = mongoose.model('books', bookSchema);
+
+export default bookModel;
